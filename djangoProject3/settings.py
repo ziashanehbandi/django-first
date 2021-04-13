6a8a6b658b5667f65d543856f2ea17b08f3896bc
ziashanehbandi <ziashanehbandi@gmail.com>
2021-04-13 22:11:57 +0430

    for posixpath

--- djangoProject3/settings.py
+++ djangoProject3/settings.py
@@ -121,9 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+#MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR.joinpath('static')
 
 MEDIA_ROOT = BASE_DIR.joinpath('media')
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = BASE_DIR / 'media'
